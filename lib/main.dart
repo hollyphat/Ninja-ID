f4b6fbc5fe2505e3a058e8479e6074a3157ea63a
Olatunji <sirhollyfat@gmail.com>
2019-12-20 15:44:07 +0100

    Create Stateful class

--- lib/main.dart
+++ lib/main.dart
@@ -90,3 +90,16 @@ class NinjaCard extends StatelessWidget {
     );
   }
 }
+
+
+class Test extends StatefulWidget {
+  @override
+  _TestState createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
